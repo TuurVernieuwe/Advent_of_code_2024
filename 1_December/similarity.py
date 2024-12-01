32ccd50dddba54d6e156065cb244a745b509cb2a
Tuur Vernieuwe <tuur.vernieuwe@student.kuleuven.be>
2024-12-01 16:39:14 +0100

    cleaned up similarity.py

--- 1_December/similarity.py
+++ 1_December/similarity.py
@@ -1,16 +1,7 @@
 with open('1_december/input.txt', 'r') as input_file:
-    values1 = []
-    values2 = []
+    values1, values2 = zip(*(line.strip().split() for line in input_file))
 
-    for line in input_file:
-        values = line.strip().split()
-        values1.append(values[0])
-        values2.append(values[1])
-
-similarity = 0
-for i in range(len(values1)):
-    mult = values2.count(values1[i])
-    similarity += int(values1[i])*mult
+similarity = sum(int(value) * values2.count(value) for value in values1)
 
 print(similarity)
 
