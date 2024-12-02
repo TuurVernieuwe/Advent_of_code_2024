b8b5b237bcc614583d5ef0eba8ee73996b8d1dfa
Tuur Vernieuwe <tuur.vernieuwe@student.kuleuven.be>
2024-12-02 12:04:40 +0100

    use def for part 1

--- 2_december/Reports.py
+++ 2_december/Reports.py
@@ -1,22 +1,18 @@
-with open('2_december/input.txt', 'r') as inputFile:
-    reports = [line.strip().split() for line in inputFile]
-
-total_safe = 0
-for report in reports:
-    safe = True
+def is_safe(report):
+    is_ascending = report[0] < report[1]
     for i in range(len(report)-1):
-        distance = abs(int(report[i+1]) - int(report[i]))
+        distance = abs(report[i+1] - report[i])
         if distance < 1 or distance > 3:
-            safe = False
-        if int(report[0]) > int(report[1]):
-            if int(report[i+1]) >= int(report[i]):
-                safe = False
-        else:
-            if int(report[i+1]) <= int(report[i]):
-                safe = False
-    if safe:
-        total_safe += 1
-        
+            return False
+        if (is_ascending and report[i+1] <= report[i]) or (not is_ascending and report[i+1] >= report[i]):
+            return False
+    return True
+
+
+with open('2_december/input.txt', 'r') as inputFile:
+    reports = [list(map(int, line.strip().split())) for line in inputFile]
+
+total_safe = sum(1 for report in reports if is_safe(report))  
 
 print(total_safe)
 
