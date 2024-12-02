c967d15a1d2494fcdf25b7c2cce5e4082f7eb4d7
Tuur Vernieuwe <tuur.vernieuwe@student.kuleuven.be>
2024-12-02 12:24:00 +0100

    cleaned up dampener

--- 2_december/Reports_with_dampener.py
+++ 2_december/Reports_with_dampener.py
@@ -1,35 +1,17 @@
 def is_safe(report):
     is_ascending = report[0] < report[1]
-    for i in range(len(report)-1):
-        distance = abs(report[i+1] - report[i])
-        if distance < 1 or distance > 3:
-            return False
-        if (is_ascending and report[i+1] <= report[i]) or (not is_ascending and report[i+1] >= report[i]):
-            return False
-    return True
+    return all(1 <= abs(report[i+1] - report[i]) <= 3 and ((is_ascending and report[i+1] > report[i]) or (not is_ascending and report[i+1] < report[i])) for i in range(len(report)-1))
 
+def can_be_safe(report):
+    if is_safe(report):
+        return True
+    else:
+        return any(is_safe(report[:i] + report[i+1:]) for i in range(len(report)))
 
 with open('2_december/input.txt', 'r') as inputFile:
     reports = [list(map(int, line.strip().split())) for line in inputFile]
 
-total_safe = 0
-for report in reports:
-    #check if initially safe
-    if is_safe(report):
-        total_safe += 1
-
-    #check if it becomes safe by removing any number
-    else:
-        for i in range(len(report)):
-            copy_report = report.copy()
-            copy_report.pop(i)
-            if is_safe(copy_report):
-                total_safe += 1
-                break
-        
-    
-
-    
+total_safe = sum(can_be_safe(report) for report in reports)
 
 print(total_safe)
 
