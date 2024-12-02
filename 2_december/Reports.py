7ea2207f0ffbab53888e0773911a4431dc63fe42
Tuur Vernieuwe <tuur.vernieuwe@student.kuleuven.be>
2024-12-02 12:24:33 +0100

    cleaned up reports

--- 2_december/Reports.py
+++ 2_december/Reports.py
@@ -1,12 +1,6 @@
 def is_safe(report):
     is_ascending = report[0] < report[1]
-    for i in range(len(report)-1):
-        distance = abs(report[i+1] - report[i])
-        if distance < 1 or distance > 3:
-            return False
-        if (is_ascending and report[i+1] <= report[i]) or (not is_ascending and report[i+1] >= report[i]):
-            return False
-    return True
+    return all(1 <= abs(report[i+1] - report[i]) <= 3 and ((is_ascending and report[i+1] > report[i]) or (not is_ascending and report[i+1] < report[i])) for i in range(len(report)-1))
 
 
 with open('2_december/input.txt', 'r') as inputFile:
